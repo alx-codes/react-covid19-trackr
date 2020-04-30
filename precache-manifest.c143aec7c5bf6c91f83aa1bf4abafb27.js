82073bbf7883c54623bee03b020fa24d222d1ded
Alejandro Figueroa <aleckzf@gmail.com>
2020-04-30 16:32:02 -0500

    Updates

---
+++ precache-manifest.c143aec7c5bf6c91f83aa1bf4abafb27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1346b3b8aa0f99cc88330e8464b364cd",
+    "url": "/alx-codes/react-covid19-trackr.git/index.html"
+  },
+  {
+    "revision": "412527df208a3f6a5a65",
+    "url": "/alx-codes/react-covid19-trackr.git/static/css/main.b9ae0ad9.chunk.css"
+  },
+  {
+    "revision": "9ea56165e1538a039b92",
+    "url": "/alx-codes/react-covid19-trackr.git/static/js/2.c62ae718.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/alx-codes/react-covid19-trackr.git/static/js/2.c62ae718.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "412527df208a3f6a5a65",
+    "url": "/alx-codes/react-covid19-trackr.git/static/js/main.d0bf4ff7.chunk.js"
+  },
+  {
+    "revision": "2da7ce3969c9eb097588",
+    "url": "/alx-codes/react-covid19-trackr.git/static/js/runtime-main.67843a71.js"
+  }
+]);
